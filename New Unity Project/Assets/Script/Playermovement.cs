1e8032c10ff4a9d95e898936ef18398b54d7caf7
CHAN ZHI HAO <163940Z@sidm.student.local>
2018-02-12 14:24:06 +0800

    Cleanup.bat / health

--- New Unity Project/Assets/Script/Playermovement.cs
+++ New Unity Project/Assets/Script/Playermovement.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Playermovement : MonoBehaviour
 {
+    [SerializeField]
+    float totalHealth = 100;
+    float health = 100;
+    float totalMana = 100;
+    float mana = 100;
+    [SerializeField]
+    GameObject healthBar, manaBar;
 
     [SerializeField]
     float movementspeed = 10;
@@ -34,22 +41,37 @@ public class Playermovement : MonoBehaviour
             jumping = true;
             jump2 = false;
         }
-            if (jumping == true)
-            {
-                transform.position += transform.up * 10 * Time.deltaTime;
-            }
-            if (transform.position.y >= 3)
-            {
-                jumping = false;
-                jump2 = true;
-            }
-            if (jump2 == true)
-            {
-                if (transform.position.y >= 0.5)
-                    transform.position -= transform.up * 10 * Time.deltaTime;
-                if (transform.position.y < 0.5)
-                    transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
-            }
-        
+        if (jumping == true)
+        {
+            transform.position += transform.up * 10 * Time.deltaTime;
+        }
+        if (transform.position.y >= 3)
+        {
+            jumping = false;
+            jump2 = true;
+        }
+        if (jump2 == true)
+        {
+            if (transform.position.y >= 0.5)
+                transform.position -= transform.up * 10 * Time.deltaTime;
+            if (transform.position.y < 0.5)
+                transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
+        }
+
+        healthBar.transform.localScale = new Vector3(health / totalHealth, 1, 1);
+        manaBar.transform.localScale = new Vector3(mana / totalMana, 1, 1);
+
+        health -= 1.0f;
+        if (health <= 0.0f)
+            health = 0.0f;
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            mana -= 10.0f;
+            if (mana <= 0.0f)
+                mana = 0.0f;
+        }
+        mana += Time.deltaTime * 3.0f;
+
     }
 }
---
+++ New Unity Project/cleanup.bat
@@ -0,0 +1,11 @@
+del /a /s "*.csproj"
+del /a /s "*.unityproj"
+del /a /s "*.sln"
+del /a /s "*.userprefs"
+
+
+rmdir /s /q Library\
+rmdir /s /q Temp\
+rmdir /s /q Keyboard\
+rmdir /s /q Build\
+rmdir /s /q .vs\
