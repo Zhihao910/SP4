ba9ff78aea97543130ff375c636c8a432f65180b
TheUnholyGod <kitsonlow99@gmail.com>
2018-02-13 15:49:56 +0800

    Audio Splitter
    The Name

---
+++ New Unity Project/Assets/Script/Audio/AudioSplitter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSplitter : MonoBehaviour {
+
+    public static AudioClip SplitAudio(int _offset, int _size, AudioClip _full)
+    {
+        AudioClip _audioclip = AudioClip.Create(_full.name + "1", _size, _full.channels, _full.frequency, false);
+        float[] _samples = new float[_size];
+        _full.GetData(_samples, _offset);
+        _audioclip.SetData(_samples, 0);
+
+        return _audioclip;
+    }
+}
