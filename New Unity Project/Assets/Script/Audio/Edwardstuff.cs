c8e1dcd9e04edcb5f0b5def0a17c14037e337e22
TheUnholyGod <kitsonlow99@gmail.com>
2018-02-17 23:30:41 +0800

    Freaking Edward's Stuffs in maingame2

---
+++ New Unity Project/Assets/Script/Audio/Edwardstuff.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edwardstuff : MonoBehaviour {
+
+    [SerializeField]
+    AudioWrapper frontpeer;
+    [SerializeField]
+    AudioWrapper backpeer;
+
+    [SerializeField]
+    AudioClip _sample;
+
+    // Use this for initialization
+    void Start()
+    {
+        frontpeer.SetAudioClip(_sample);
+        frontpeer.StartPlaying();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //Debug.Log(curr.GetClipName());
+	}
+
+    public static void Swap()
+    {
+    }
+    public void Play()
+    {
+        frontpeer.StartPlaying();
+    }
+
+    //public void PrintTime()
+    //{
+    //    Debug.Log(_sample.time);
+    //}
+
+    //public void PrintIntro()
+    //{
+    //    Debug.Log("Intro");
+    //}
+
+    //public void PrintVerse()
+    //{
+    //    Debug.Log("Verse");
+    //}
+
+    //public void PrintBreak()
+    //{
+    //    Debug.Log("Break");
+    //}
+
+    //public void PrintBuildUp()
+    //{
+    //    Debug.Log("BuildUp");
+    //}
+
+    //public void PrintDrop()
+    //{
+    //    Debug.Log("Drop");
+    //}
+
+    public float TimeNow()
+    {
+        return frontpeer.TimeNow();
+    }
+}
