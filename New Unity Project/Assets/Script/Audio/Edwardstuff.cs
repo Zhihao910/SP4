14bbb98fe25fe7217cd0bbcb30444b0182f75df9
Psidra <xdusk37@gmail.com>
2018-02-18 14:19:44 +0800

    Attempt to merge my beat detection with game, will tweak overtime, just testing now

--- New Unity Project/Assets/Script/Audio/Edwardstuff.cs
+++ New Unity Project/Assets/Script/Audio/Edwardstuff.cs
@@ -12,21 +12,114 @@ public class Edwardstuff : MonoBehaviour {
     [SerializeField]
     AudioClip _sample;
 
+    [SerializeField]
+    GameObject Projectile;
+
+    private bool spawnBass;
+    private bool spawnKick;
+    private bool spawnMelody;
+    private bool spawnCenter;
+    private bool spawnHigh;
+
     // Use this for initialization
     void Start()
     {
         frontpeer.SetAudioClip(_sample);
         frontpeer.StartPlaying();
+
+        spawnBass = false;
+        spawnKick = false;
+        spawnMelody = false;
+        spawnCenter = false;
+        spawnHigh = false;
     }
 
     // Update is called once per frame
     void Update () {
         //Debug.Log(curr.GetClipName());
+
+        //Vector2 randomPosition = new Vector2(15, Random.Range(-1, -5));
+
+        // BASS
+        if (SpawnEffect._spawnBass)
+        {
+            if (!spawnBass)
+            {
+                Instantiate(Projectile, new Vector2(15, 0), Quaternion.identity);
+
+                spawnBass = true;
+            }
+        }
+        else
+        {
+            spawnBass = false;
+        }
+
+        // KICK
+        if (SpawnEffect._spawnKick)
+        {
+            if (!spawnKick)
+            {
+                Instantiate(Projectile, new Vector2(15, -1), Quaternion.identity);
+
+                spawnKick = true;
+            }
+        }
+        else
+        {
+            spawnKick = false;
+        }
+
+        // ACTUALLY ALSO PART OF VOCALS AND INSTRUMENTS
+        if (SpawnEffect._spawnCenter)
+        {
+            if (!spawnCenter)
+            {
+                Instantiate(Projectile, new Vector2(15, -2), Quaternion.identity);
+
+                spawnCenter = true;
+            }
+        }
+        else
+        {
+            spawnCenter = false;
+        }
+
+        // VOCALS AND INSTRUMENTS
+        if (SpawnEffect._spawnMelody)
+        {
+            if (!spawnMelody)
+            {
+                Instantiate(Projectile, new Vector2(15, -3), Quaternion.identity);
+
+                spawnMelody = true;
+            }
+        }
+        else
+        {
+            spawnMelody = false;
+        }
+
+        // CHALKBOARD SCREECHING
+        if (SpawnEffect._spawnHigh)
+        {
+            if (!spawnHigh)
+            {
+                Instantiate(Projectile, new Vector2(15, -4), Quaternion.identity);
+
+                spawnHigh = true;
+            }
+        }
+        else
+        {
+            spawnHigh = false;
+        }
 	}
 
     public static void Swap()
     {
     }
+
     public void Play()
     {
         frontpeer.StartPlaying();
