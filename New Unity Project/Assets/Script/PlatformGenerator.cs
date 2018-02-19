3bada695d67d323e7f44420611b692cace6f34bc
CHAN ZHI HAO <163940Z@sidm.student.local>
2018-02-19 13:16:30 +0800

    generatePlatform

--- New Unity Project/Assets/Script/PlatformGenerator.cs
+++ New Unity Project/Assets/Script/PlatformGenerator.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlatformGenerator : MonoBehaviour
 {
     [SerializeField]
-    GameObject platform;
+    GameObject[] platform;
+    List<GameObject> movingPlatforms = new List<GameObject>();
     int numOfPlatforms = 5;
     bool stopSpawn = true;
     float spawnTimer = 3;
@@ -24,15 +25,29 @@ public class PlatformGenerator : MonoBehaviour
             stopSpawn = false;
             GeneratePlatform();
         }
+        UpdatePlatform();
     }
 
     void GeneratePlatform()
     {
         for (int i = 0; i < numOfPlatforms; ++i)
         {
-            Vector2 position = new Vector2(Random.Range(-8, 8), -5);
-            Instantiate(platform, position, Quaternion.identity);
-            platform.SetActive(true);
+            Vector2 randomPosition = new Vector2(Random.Range(-8, 8), -10);
+           
+            movingPlatforms.Add(Instantiate(platform[Random.Range(0,2)], randomPosition, Quaternion.identity)); //Create platform at different position
+            platform[0].SetActive(true); //Show the platform
+        }
+    }
+
+    void UpdatePlatform()
+    {
+        foreach (GameObject go in movingPlatforms)
+        {
+            go.transform.Translate(0, 1 * Time.deltaTime, 0);
+            if (go.transform.position.y >= -6)
+            {
+                go.transform.position = new Vector3(go.transform.position.x, -6, 0);
+            }
         }
     }
 }
