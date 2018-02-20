abc1cec2e51d7dffe2d17ac651d587c884b58568
TheUnholyGod <kitsonlow99@gmail.com>
2018-02-20 18:04:47 +0800

    Added New Future Base State

---
+++ New Unity Project/Assets/Script/States/BaseState1.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseState1 : MonoBehaviour {
+
+    public delegate void Attack();
+
+    enum Type
+    {
+        BASS_TYPE,
+        KICK_TYPE,
+        GENERAL_TYPE,
+        TYPE_NUM,
+    };
+
+    public AudioPeerManager m_audioManager;
+
+    Attack[] _attacks = new Attack[(int)(Type.TYPE_NUM)];
+
+    [SerializeField]
+    string m_clipname;
+
+    bool m_Run = false;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        foreach(Attack a in _attacks)
+        {
+            //Condition
+            //if()
+            //a();
+        }
+	}
+
+    public void AddAttack(int _go,Attack _function)
+    {
+        _attacks[_go] = _function;
+    }
+
+
+    public string GetClipName()
+    {
+        return m_clipname;
+    }
+
+    public void SetClipName(string _clipname)
+    {
+        m_clipname = _clipname;
+    }
+
+    public void StopRun()
+    {
+        m_Run = false;
+    }
+}
