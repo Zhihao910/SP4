dba2c4269a7ba40483b805fe0b95a8830ac9278f
LEE YONG HAN <162608U@sidm.student.local>
2018-02-20 16:44:39 +0800

    Added Healthpack

---
+++ New Unity Project/Assets/Script/powerupspawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerupspawner : MonoBehaviour {
+    [SerializeField]
+    GameObject Powerup;
+
+    float spawntime;
+
+    // Use this for initialization
+    void Start()
+    {
+        spawntime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawntime += Time.deltaTime;
+        Vector2 randomPosition = new Vector2(Random.Range(-5, 5), 0);
+        if (spawntime >= 10)
+        {
+            Instantiate(Powerup, randomPosition, Quaternion.identity);
+            spawntime = 0;
+        }
+    }
+}
