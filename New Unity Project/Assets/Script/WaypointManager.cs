4960d1edb80f68e4669262cd03724641b480d0ef
CHAN ZHI HAO <163940Z@sidm.student.local>
2018-02-21 15:05:20 +0800

    Overworld

--- New Unity Project/Assets/Script/WaypointManager.cs
+++ New Unity Project/Assets/Script/WaypointManager.cs
@@ -1,47 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WaypointManager : MonoBehaviour
 {
     public GameObject[] Waypoint;
     public int num = 0;
+    Vector3 target;
 
     float minDistance = 1;
     float speed = 2;
 
-    bool go = true;
+    bool go = false;
+    bool confirmEnter = false;
+
+    private Animator animator;
 
     // Use this for initialization
     void Start()
     {
-
+        animator = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float distance = Vector3.Distance(gameObject.transform.position, Waypoint[num].transform.position);
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+            go = true;
+        }
 
         if (go)
         {
-            if (distance > minDistance)
-                Move();
-            else
-            {
-                if (num + 1 == Waypoint.Length)
-                    num = 0;
-                else num++;
-            }
+            animator.SetInteger("States", 2);
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, target, speed * Time.deltaTime);
+            print(gameObject.transform.position);
+            if (gameObject.transform.position == target)
+                animator.SetInteger("States", 3);
         }
-
     }
 
-    public void Move()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        gameObject.transform.LookAt(Waypoint[num].transform.position);
-        gameObject.transform.position += gameObject.transform.forward * speed * Time.deltaTime;
-
-
+        if (other.CompareTag("Waypoint"))
+        {
+            print("TouchedWaypoint");
+            SceneManager.LoadScene("MainMenu");
+        }
+        else if (other.CompareTag("Waypoint1"))
+        {
+            print("TouchedWaypoint1");
+            SceneManager.LoadScene("Help");
+        }
+        else if (other.CompareTag("Waypoint2"))
+        {
+            print("TouchedWaypoint2");
+            SceneManager.LoadScene("MainGame 1");
+        }
+        else if (other.CompareTag("Waypoint3"))
+        {
+            print("TouchedWaypoint3");
+            SceneManager.LoadScene("GameOver");
+        }
     }
 }
