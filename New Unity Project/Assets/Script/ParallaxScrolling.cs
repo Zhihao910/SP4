86bb195d2a508afd046457bdc8ae5bfe3340729c
Zhihao <>
2018-02-21 23:44:07 +0800

    Parallax background

---
+++ New Unity Project/Assets/Script/ParallaxScrolling.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScrolling : MonoBehaviour
+{
+    public float speed;
+    Vector3 bgPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        bgPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        parallaxBackground();
+    }
+
+    void parallaxBackground()
+    {
+        transform.Translate((new Vector3(-1, 0, 0)) * speed * Time.deltaTime);
+        if (transform.position.x < -41.7)
+            transform.position = bgPos;
+    }
+}
