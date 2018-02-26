50e1a8c78d43fff9dcd597df9ddad9f39b2b2d12
TheUnholyGod <kitsonlow99@gmail.com>
2018-02-26 14:09:53 +0800

    Update UI with Song Preview

---
+++ New Unity Project/Assets/Script/SongSelectionUI/SongManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongManager : MonoBehaviour {
+    [SerializeField]
+    AudioWrapper frontpeer;
+    [SerializeField]
+    AudioWrapper backpeer;
+
+    Dictionary<string, AudioClip> m_audioclipmap = new Dictionary<string, AudioClip>();
+
+    // Use this for initialization
+    void Start () {
+	    foreach(string s in GetComponent<UIController>().songs)
+        {
+            m_audioclipmap.Add(s, (Resources.Load("Audio/" + s) as AudioClip));
+        }	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      
+    }
+
+    public void Swap(string _name)
+    {
+        AudioWrapper ap = frontpeer;
+        frontpeer.SetFadeOut();
+        frontpeer = backpeer;
+        frontpeer.SetAudioClip(m_audioclipmap[_name]);
+        frontpeer.SetFadeIn();
+        frontpeer.StartPlaying();
+        backpeer = ap;
+    }
+
+    public void Play()
+    {
+        frontpeer.StartPlaying();
+    }
+}
