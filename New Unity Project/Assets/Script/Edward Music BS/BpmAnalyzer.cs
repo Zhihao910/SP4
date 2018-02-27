c79bd5e1a76624d75a5922abbfb7bf29d5f493d8
TheUnholyGod <kitsonlow99@gmail.com>
2018-02-27 19:20:34 +0800

    UI Updates
    Fking .toString() and .name is 2 diff things u faggot why waste so much time on that

--- New Unity Project/Assets/Script/Edward Music BS/BpmAnalyzer.cs
+++ New Unity Project/Assets/Script/Edward Music BS/BpmAnalyzer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,16 +13,30 @@ public class BpmAnalyzer : MonoBehaviour
     [SerializeField]
     AudioPeerManager _apm;
 
-    private int _bpm = 0;
+    public int _bpm = 0;
     private float _bpmTime = 0.0f;
 
     // Dont need to call this before calling getbpm/getbeattime
     // I did it anyway
     public void FindBpm()
     {
+        //if (_audioSource.clip == null && _apm == null)
+        //{
+        //    //_audioSource = GetComponent<SongManager>().frontpeer.GetComponent<AudioSource>();
+        //}
+
+        //_audioSource = GetComponent<SongManager>().frontpeer.GetComponent<AudioSource>();
+
         if (_audioSource.clip == null)
         {
-            _bpm = UniBpmAnalyzer.AnalyzeBpm(_apm._sample2);
+            if (_apm == null)
+            {
+                _bpm = UniBpmAnalyzer.AnalyzeBpm(GetComponent<SongManager>().frontpeer.GetComponent<AudioSource>().clip);
+            }
+            else
+            {
+                _bpm = UniBpmAnalyzer.AnalyzeBpm(_apm._sample2);
+            }
 
             if (_bpm < 0)
             {
@@ -69,5 +84,55 @@ public class BpmAnalyzer : MonoBehaviour
         }
 
         return _bpmTime;
+    }
+
+    public void ReadBpm(string _name)
+    {
+        if (Saving.LoadingFromFile("MusicData.txt", (List<string> _data) =>
+        {
+            // See if song exists
+            string checker = "<name>" + _name;
+            if (_data.Contains(checker as string))
+            {
+                print("Song Data found, reading now...");
+            }
+            else
+            {
+                print("could not find song:" + _name);
+
+                print(checker);
+                print("<name>Quintino - Go Hard (Official Music Video)");
+                print(checker.Equals("<name>Quintino - Go Hard (Official Music Video)"));
+                print(_data.Contains("<name>Quintino - Go Hard (Official Music Video)"));
+
+                return false;
+            }
+
+            List<string> songData = new List<string>();
+            songData = _data.GetRange(_data.IndexOf("<name>" + _name), (_data.IndexOf(_name + "end") - _data.IndexOf("<name>" + _name)));
+
+            // FIND BPM
+            List<string> bpmData = new List<string>();
+            bpmData = songData.GetRange(songData.IndexOf("<bpm>"), (songData.IndexOf("</bpm>") - songData.IndexOf("<bpm>")));
+            bpmData.Remove("<bpm>");
+            string bpmString = "";
+            foreach (string num in bpmData)
+                bpmString += num;
+
+            this.SetBpm(Convert.ToInt32(bpmString, System.Globalization.CultureInfo.InvariantCulture.NumberFormat));
+
+            return true;
+        }
+    ))
+        {
+            print("Successfully loaded from file.");
+
+            return;
+        }
+        else
+        {
+            print("Song does not exist or error in reading file. Will detect.");
+            return;
+        }
     }
 }
